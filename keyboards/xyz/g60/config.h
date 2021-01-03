ceaab6b600d73b4142077ac065f0875d07b9d2f1
Ilya Zhuravlev <whatever@xyz.is>
2021-01-03 08:56:12 -0500

    xyz/g60: update for layout options & latest vial

--- keyboards/xyz/g60/config.h
+++ keyboards/xyz/g60/config.h
@@ -63,3 +63,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_ONESHOT
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
