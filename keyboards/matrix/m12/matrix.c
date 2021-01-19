bc049a0fbf1afc0a31712eb44fdd572077683ac4
Ilya Zhuravlev <whatever@xyz.is>
2021-01-18 19:03:03 -0500

    add matrix/m12

---
+++ keyboards/matrix/m12/matrix.c
@@ -0,0 +1,80 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "util.h"
+#include "matrix.h"
+#include "debounce.h"
+#include "quantum.h"
+
+static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
+static const pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
+
+static void select_col(uint8_t col) {
+    setPinOutput(col_pins[col]);
+    writePinHigh(col_pins[col]);
+}
+
+static void unselect_col(uint8_t col) { setPinInputLow(col_pins[col]); }
+
+static void unselect_cols(void) {
+    for (uint8_t x = 0; x < MATRIX_COLS; x++) {
+        setPinInputLow(col_pins[x]);
+    }
+}
+
+static void init_pins(void) {
+    unselect_cols();
+    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
+        setPinInputLow(row_pins[x]);
+    }
+}
+
+static bool read_rows_on_col(matrix_row_t current_matrix[], uint8_t current_col) {
+    bool matrix_changed = false;
+
+    // Select col and wait for col selecton to stabilize
+    select_col(current_col);
+    matrix_io_delay();
+
+    // For each row...
+    for (uint8_t row_index = 0; row_index < MATRIX_ROWS; row_index++) {
+        // Store last value of row prior to reading
+        matrix_row_t last_row_value    = current_matrix[row_index];
+        matrix_row_t current_row_value = last_row_value;
+
+        // Check row pin state
+        if (readPin(row_pins[row_index]) != 0) {
+            // Pin LO, set col bit
+            current_row_value |= (MATRIX_ROW_SHIFTER << current_col);
+        } else {
+            // Pin HI, clear col bit
+            current_row_value &= ~(MATRIX_ROW_SHIFTER << current_col);
+        }
+
+        // Determine if the matrix changed state
+        if ((last_row_value != current_row_value)) {
+            matrix_changed |= true;
+            current_matrix[row_index] = current_row_value;
+        }
+    }
+
+    // Unselect col
+    unselect_col(current_col);
+
+    return matrix_changed;
+}
+
+void matrix_init_custom(void) {
+    // initialize key pins
+    init_pins();
+}
+
+uint8_t matrix_scan_custom(matrix_row_t current_matrix[]) {
+    bool changed = false;
+
+    // Set col, read rows
+    for (uint8_t current_col = 0; current_col < MATRIX_COLS; current_col++) {
+        changed |= read_rows_on_col(current_matrix, current_col);
+    }
+
+    return (uint8_t)changed;
+}
